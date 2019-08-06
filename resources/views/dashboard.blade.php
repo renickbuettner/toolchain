485052dcd07635bd39a58a011952acda4dd2a891
Renick Büttner <renick@staffbase.com>
2019-08-06 16:11:45 +0200

    restore dashboard view dummy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,24 @@
-@extends('layout')
-
+@extends('layouts/master')
 
 @section('title', 'Dashboard')
 
+@section('body')
 
-@section('content')
 
-        <div class="title m-b-md">
-            Dashborad
-        </div>
+<section class="row m-0 ui">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-3 sidebar">
+        <header>
+            <h1>Toolchain</h1>
+            <h5>Beat your toolchain mess</h5>
+        </header>
+
+        <section class="ui-group">
+            <a href="#" class="ui-btn active">All services</a>
+            <a href="#" class="ui-btn">Add a service</a>
+            <a href="#" class="ui-btn">Request approval</a>
+            <a href="#" class="ui-btn">Help center</a>
+        </section>
+    </div>
+</section>
 
 @endsection
