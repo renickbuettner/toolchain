e79cba411ee970512ae3f8f7c6c988be2d609e6c
Renick Büttner <renick@staffbase.com>
2019-08-17 13:13:22 +0200

    fix layout issue on dashboard

--- resources/views/partials/service.blade.php
+++ resources/views/partials/service.blade.php
@@ -3,7 +3,7 @@
  --}}
 
 <section class="service col-12 col-md-4" data-id="{{$service->getSlug()}}">
-    <div class="card">
+    <div class="card mb-4">
         <div class="card-header">
             <h4 class="m-0">{{$service->getTitle()}}</h4>
         </div>
