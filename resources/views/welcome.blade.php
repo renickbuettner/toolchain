dc9ac0c11e537304c14833be43b19a2bf7813bbc
Renick Büttner <renick.buettner@me.com>
2019-08-18 15:08:18 +0200

    add first draft

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,10 @@
 
 @section('content')
 
-        <div class="title m-b-md">
-            Toolchain
+    <section class="home-page">
+        <div class="wrapper">
+            <h1 class="title" data-text="TOOLCHAIN">TOOLCHAIN</h1>
         </div>
+    </section>
 
 @endsection
