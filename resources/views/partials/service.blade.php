eb532c824d2e6ea93f826ead59263b119b3bccbc
zumpi <jonas@ostec.de>
2019-08-19 10:11:40 +0200

    dashboard service styling progress

--- resources/views/partials/service.blade.php
+++ resources/views/partials/service.blade.php
@@ -3,14 +3,14 @@
  --}}
 
 <section class="service col-12 col-md-4" data-id="{{$service->getSlug()}}">
-    <div class="card mb-4">
+    <div class="card mb-4 card-style">
         @if($service->getIcon() !== '')
         <img src="{{$service->getIcon()}}" class="img-thumbnail">
         @endif
         <div class="card-header">
             <h4 class="m-0">{{$service->getTitle()}}</h4>
         </div>
-        <div class="card-body">
+        <div class="card-body card-style">
             <a href="{{$service->getInternalUrl()}}" class="btn btn-secondary">View details</a>
             <a href="{{$service->getUrl()}}" class="btn btn-primary" target="_blank">Open</a>
         </div>
