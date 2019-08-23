0dd9cc6339bc408b3308e0bd3cce99e3537fdd69
zumpi <jonas@ostec.de>
2019-08-23 11:24:51 +0200

    Add BlackboxTest && testCreateServiceMiddleware

--- resources/js/editor/serviceEditor.js
+++ resources/js/editor/serviceEditor.js
@@ -46,9 +46,7 @@ export class ServiceEditor {
             this._slug = service.slug;
             this._icon.value = service.icon;
             this._description.value = service.description;
-            this._description.value = service.description;
             this._wysiwyg.setContent(service.description);
-
         } catch (e) {
             console.debug(e);
             throw new Error('Can not load service');
